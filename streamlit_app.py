46f7d3c67b54561138de27f95d2f746449d64a0f
0nlykyle <99232687+0nlykyle@users.noreply.github.com>
2022-07-29 04:50:57 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+
+from tkinter import PAGES
+import streamlit as st
+import streamlit_book as stb
+from PIL import Image, ImageEnhance
+# Set wide display
+st.set_page_config()
+
+# Set multipage
+stb.set_chapter_config(path="pages", save_answers=True)
+
+button1 = st.sidebar.button("Log in")
+if button1:
+    with st.sidebar.form(key='my_form'):
+        username = st.text_input('Username')
+        password = st.text_input('Password')
+        st.form_submit_button('Log in')
+
+button2 = st.sidebar.button("Sign up")
+if button2:
+    with st.sidebar.form(key='my_form'):
+        name = st.text_input('ชื่อ-สกุล')
+        username = st.text_input('ชื่อผู้ใช้')
+        email = st.text_input('อีเมล')
+        password = st.text_input('รหัสผ่าน')
+        confirmpassword = st.text_input('ยืนยันรหัสผ่าน')
+        st.form_submit_button('sign up')
+
+
+
