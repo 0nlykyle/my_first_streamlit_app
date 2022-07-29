44c12ebd098904fc351321024bf52c8342974a8c
0nlykyle <99232687+0nlykyle@users.noreply.github.com>
2022-07-29 20:21:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ if button2:
 hide_st_style = """
             <style>
             footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
